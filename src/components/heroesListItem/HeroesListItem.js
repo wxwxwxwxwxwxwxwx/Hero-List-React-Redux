a23c4b87c32a71d15b497b1c97bcb1e04108ceef
Anton <ushkov6@mail.ru>
2021-12-04 13:38:36 +0400

    add css transiton

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -32,7 +32,8 @@ const HeroesListItem = ({name, description, element, onDelete}) => {
                 <h3 className="card-title">{name}</h3>
                 <p className="card-text">{description}</p>
             </div>
-            <span onClick={onDelete} className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
+            <span onClick={onDelete} 
+                  className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
                 <button 
                     type="button" 
                     className="btn-close btn-close" 
