a527555528d366d8678c6ba4760938ea3c3da364
Anton <ushkov6@mail.ru>
2021-12-05 18:28:52 +0400

    add enhancer + spinner fix

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,23 @@ import { createStore, combineReducers } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore(combineReducers({heroes, filters}),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = createStore => (...args) => {
+    const store = createStore(...args);
+
+    const oldDispatch = store.dispatch;
+    store.dispatch = action => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action
+            });
+        }
+        return oldDispatch(action)
+    }
+    return store;
+}
+
+const store = createStore(combineReducers({heroes, filters}), enhancer);
 
 export default store;
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
