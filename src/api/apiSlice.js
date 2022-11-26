a28e46555850deea9ce6d507946527ed0b8f9891
Anton <ushkov6@mail.ru>
2022-11-26 13:58:26 +0400

    change link v2

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://wxwxwxwxwxwxwxwx.github.io/Hero-List-React-Redux'}),
     tagTypes: ['Heroes'],
     endpoints: builder => ({
         getHeroes: builder.query({
